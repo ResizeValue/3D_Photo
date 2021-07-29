54e5fa5a7dea3c53f8267f49f9de57a121f6607b
user <user@DESKTOP-FQJQCDG>
2021-07-29 19:15:32 +0300

    Relese ver. 1.0

--- 3D_Photo/HTML_Program.cs
+++ 3D_Photo/HTML_Program.cs
@@ -11,7 +11,7 @@ namespace _3D_Photo
     {
 
         string script;
-
+        
         void Generate_Script(List<Photo_> photos, double sens)
         {
             string photos_string = "";
@@ -19,6 +19,15 @@ namespace _3D_Photo
             {
                 photos_string += $"\"data:image/jpg;base64,{Convert.ToBase64String(File.ReadAllBytes(photo.Path))}\",";
             }
+            string arrow = "";
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream("_3D_Photo.left_arr.png"))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                arrow = Convert.ToBase64String(buffer);
+            }
+
             photos_string = photos_string.Remove(photos_string.Length - 1, 1);
             script = $@"<head>
                             <meta charset=""UTF-8"">
@@ -27,12 +36,16 @@ namespace _3D_Photo
                             <title>Wolf Griman 3Dex</title>
 
                             <script>
-                                //data:image/jpg;base64,
+                                
                                 let photos = [{photos_string}];
 
                                 let x = 0;
 
                                 var sens = {sens};
+
+                                let IsScroll = false;
+                                var autoScrollDirection = 0;
+                                let auto_scroll_timer;
 
                                 let point = null;
                                 function stopDrag(){{
@@ -40,6 +53,8 @@ namespace _3D_Photo
                                 }}
                                 var click = false;
                                 function imgBtnDown(){{
+                                    if(IsScroll)
+                                    stop_Auto_Scroll();
                                     click = true;
                                     document.getElementById('main_img').style.cursor = ""grabbing"";
                                     point = [event.pageX, event.pageY];
@@ -48,12 +63,12 @@ namespace _3D_Photo
                                     {{
                                         if (!click) return;
 
-                                        if (point[0] - event.pageX > sens){{
+                                        if (point[0] - event.pageX > (100/sens)){{
                                         x++;
                                         if (x == photos.length) x = 0;
                                         changeImg();
                                     }}
-                                    if(point[0] - event.pageX < -sens){{
+                                    if(point[0] - event.pageX < -(100/sens)){{
                                         x--;
                                         if (x == -1) x = photos.length - 1;
                                         changeImg();
@@ -64,14 +79,16 @@ namespace _3D_Photo
                                 function changeImg()
                                 {{
                                     document.getElementById('main_img').setAttribute('src', photos[x]);
-                                    console.log(x);
-                                    point = [event.pageX, event.pageY];
-                                }}
-
-                                function imgLoad(){{
-                                            changeImg();
-                                            console.log(""Loaded"")
+                                    if(autoScrollDirection == 0){{
+                                        if(is_touched){{
+                                            var str = event.changedTouches;
+                                            var touch = str[str.length-1];
+                                            point = [touch.pageX, touch.pageY];
                                         }}
+                                        else
+                                            point = [event.pageX, event.pageY];
+                                    }}
+                                }}
 
                                 function imgBtnUp()
                                 {{
@@ -79,20 +96,145 @@ namespace _3D_Photo
                                     document.getElementById('main_img').style.cursor = ""grab"";
                                     event.stopPropagation();
                                 }}
+
+                                function stop_Auto_Scroll(){{
+                                    autoScrollDirection = 0;
+                                    IsScroll = false;
+                                    document.getElementById(""auto_text"").style.color = ""black"";
+                                    clearInterval(auto_scroll_timer);
+                                }}
+
+                                function arrow_Click()
+                                {{
+                                    stop_Auto_Scroll();
+                                    document.getElementById(""auto_text"").style.color = ""blue"";
+                                    if(event.currentTarget.id == ""left""){{
+                                        autoScrollDirection = -1;
+                                    }}
+                                    else{{
+                                        autoScrollDirection = 1;
+                                    }}
+                                    IsScroll = true;
+                                    auto_scroll_timer = setInterval(()=>{{
+                                        x += autoScrollDirection;
+                                        if (x == photos.length) x = 0;
+                                        else if (x == -1) x = photos.length - 1;
+                                        changeImg();
+                                    }},40)
+                                }}
+
+                                function touch_init(){{
+                                    var el = document.getElementById(""main_img"");
+                                    el.addEventListener(""touchstart"", touchstart, false);
+                                    el.addEventListener(""touchcancel"", imgBtnUp, false);
+                                    el.addEventListener(""touchmove"", touchmove, false);
+                                }}
+
+                                let is_touched = false
+                                function touchstart(){{
+                                    var str = event.changedTouches;
+                                    var touch = str[str.length-1];
+                                    point = [touch.pageX,touch.pageY];
+                                    is_touched = true;
+                                }}
+
+                                function touchmove(){{
+                                    var str = event.changedTouches;
+                                    var touch = str[str.length-1];
+                                    if (point[0] - touch.pageX > (100/sens)){{
+                                        x++;
+                                        if (x == photos.length) x = 0;
+                                        changeImg();
+                                    }}
+                                    if(point[0] - touch.pageX < -(100/sens)){{
+                                        x--;
+                                        if (x == -1) x = photos.length - 1;
+                                        changeImg();
+                                    }}
+                                    
+                                }}
+
+                                function touchcancel(){{
+                                    is_touched = false;
+                                }}
                             </script>
 
                             <style>
                                 .container{{
                                     display: flex;
-                                    margin-left: auto;
-                                    margin-right: auto;
+
+                                    justify-content: center;
+                                }}
+
+                                .auto_container{{
+                                    display: flex;
+                                    flex-direction: column;
+                                    justify-items: center;
+                                    margin-left: -160px;
+                                    z-index: 2;
+                                    align-self: flex-end;
+                                    background-color: rgba(159, 89, 206, 0.199);
+                                    margin-bottom: 10px;
+                                    border-radius: 25%;
+                                }}
+                                
+                                #auto_text{{
+                                    cursor:default;
+                                    text-align: center;
+                                    font-size: 40px;
+                                    font-weight: 900;
+                                }}
+
+                                @media screen and (max-width: 600px) {{
+                                    #main_img{{
+                                        width: 475px;
+                                    }}
+                                }}
+                                @media screen and (max-width: 400px) {{
+                                    #main_img{{
+                                        width: 300px;
+                                    }}
+                                }}
+                                @media screen and (max-height: 800px) {{
+                                    #main_img{{
+                                        height: 600px;
+                                    }}
+                                }}
+
+                                .arrow_container{{
+                                    background-color: transparent;
+                                    border-radius: 25%;
+                                }}
+                                .arrow{{
+                                    width: 75;
+                                    height: 40;
+                                }}
+                                .arrow_back{{
+                                    transform: rotate(180deg);
+                                }}
+
+                                .arrow_container:hover{{
+                                    border: 1px solid black;
                                 }}
                             </style>
                         </head>
-                        <body>
-                            <div>
+                        <body onload=""touch_init()"">
+                            <div class=""container"">
                                 <img id=""main_img"" style=""cursor: grab; "" ondragstart=""stopDrag()"" onmouseout=""imgBtnUp()"" onmousedown=""imgBtnDown()""
-                                onmouseup = ""imgBtnUp()"" onmousemove = ""imgMove()"" class=""container"" src=""data:image/jpg;base64,{Convert.ToBase64String(File.ReadAllBytes(photos[0].Path))}"" >
+                                onmouseup = ""imgBtnUp()"" onmousemove = ""imgMove()"" src=""data:image/jpg;base64,{Convert.ToBase64String(File.ReadAllBytes(photos[0].Path))}"" >
+                            <div class=""auto_container"">
+                                    <div id =""auto_text"" >Auto</div>
+ 
+                                     <div style = ""display: flex; flex-direction: row;"">
+  
+                                          <div id = ""left"" class=""arrow_container"" onclick=""arrow_Click()"">
+                                            <img class=""arrow"" src=""data:image/png;base64,{arrow}"">
+                                        </div>
+                                        <div id = ""right"" class=""arrow_container"" onclick=""arrow_Click()"">
+                                            <img class=""arrow arrow_back"" src=""data:image/png;base64,{arrow}"">
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </body>";
         }
--- 3D_Photo/MainWindow.xaml.cs
+++ 3D_Photo/MainWindow.xaml.cs
@@ -29,8 +29,7 @@ namespace _3D_Photo
         
         int scroll_direction = 0;
         bool auto_scroll_enabled = false;
-        int curIndex = -1;
-        double sens = 5;
+        double sens = 50;
         public MainWindow()
         {
             InitializeComponent();
@@ -43,31 +42,51 @@ namespace _3D_Photo
 
             if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                Fill_ListBox(dialog.SelectedPath);
-                empty_model_btn.Visibility = Visibility.Hidden;
+                if (Fill_ListBox(dialog.SelectedPath))
+                {
+                    empty_model_btn.Visibility = Visibility.Hidden;
+                    no_active_model_tb.Visibility = Visibility.Hidden;
+                    no_photo.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    MessageBox.Show("По данному пути не найдено нужных файлов!");
+                    empty_model_btn.Visibility = Visibility.Visible;
+                    no_active_model_tb.Visibility = Visibility.Visible;
+                    no_photo.Visibility = Visibility.Visible;
+                }
             }
         }
 
-        void Fill_ListBox(string path)
+        bool Fill_ListBox(string path)
         {
             images.Clear();
-            DirectoryInfo info = new DirectoryInfo(path);
-
-            foreach (var file in info.GetFiles())
+            try
             {
-                if(file.Name[0] == '0' && file.Name[1] == '_')
-                {
-                    string[] tmp = file.Name.Split(new Char[] { '_', '.' });
-                    images.Add(new Photo_() { Name = file.Name, Path = file.FullName, Position = Int32.Parse(tmp[1]) });
-                }
+                DirectoryInfo info = new DirectoryInfo(path);
 
+                foreach (var file in info.GetFiles())
+                {
+                    if (file.Name[0] == '0' && file.Name[1] == '_')
+                    {
+                        string[] tmp = file.Name.Split(new Char[] { '_', '.' });
+                        images.Add(new Photo_() { Name = file.Name, Path = file.FullName, Position = Int32.Parse(tmp[1]) });
+                    }
+
+                }
+            }
+            catch
+            {
             }
             images_box.ItemsSource = null;
+            img.Source = null;
+            if (images.Count < 1) return false;
+
+            
             images.Sort(delegate (Photo_ photo1, Photo_ photo2) { return photo1.Position.CompareTo(photo2.Position); });
             images_box.ItemsSource = images;
             images_box.SelectedIndex = 0;
-            no_active_model_tb.Visibility = Visibility.Hidden;
-            no_photo.Visibility = Visibility.Hidden;
+            return true;
         }
 
         private void OpenFolder_Click(object sender, RoutedEventArgs e)
@@ -93,6 +112,7 @@ namespace _3D_Photo
 
         private void img_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            
             if (auto_scroll_enabled) Clear_Scroll();
             click = true;
             point = e.GetPosition(img);
@@ -107,7 +127,7 @@ namespace _3D_Photo
         {
             if (click)
             {
-                if(point.X + sens <= e.GetPosition(img).X)
+                if(point.X + (100/sens) <= e.GetPosition(img).X)
                 {
                     if(images_box.SelectedIndex == 0)
                     {
@@ -120,7 +140,7 @@ namespace _3D_Photo
                     img.Source = new BitmapImage(new Uri(((Photo_)images_box.SelectedItem).Path));
                     point = e.GetPosition(img);
                 }
-                else if (point.X - sens >= e.GetPosition(img).X)
+                else if (point.X - (100 / sens) >= e.GetPosition(img).X)
                 {
                     if (images_box.SelectedIndex == images_box.Items.Count - 1)
                     {
@@ -154,10 +174,10 @@ namespace _3D_Photo
             else
             {
                 images_box.SelectedIndex += scroll_direction;
-                if (images_box.SelectedIndex == images_box.Items.Count - 1) { 
+                if (images_box.SelectedIndex == images_box.Items.Count-1) { 
                     images_box.SelectedIndex = 0;
                 }
-                else if (images_box.SelectedIndex == 0) {
+                else if (images_box.SelectedIndex == -1) {
                     images_box.SelectedIndex = images_box.Items.Count - 1;
                 }
                 img.Source = new BitmapImage(new Uri(((Photo_)images_box.SelectedItem).Path));
@@ -166,31 +186,47 @@ namespace _3D_Photo
 
         void Img_Timer_Change()
         {
-            while (true)
+            while (auto_scroll_enabled)
             {
-                if (!auto_scroll_enabled) break;
                 Print_Image(0);
                 Thread.Sleep(40);
             }
-            
+           
         }
 
         private void auto_scroll_Disabled()
         {
-            auto_scroll_enabled = false;
+            try
+            {
+                auto_scroll_enabled = false;
+                if (scroll_thread != null)
+                {
+                    Console.WriteLine("Abort");
+                    scroll_thread.Abort();
+                    scroll_thread = null;
+                }
+            }
+            catch { }
         }
 
         private void export_HTML_Click(object sender, RoutedEventArgs e)
         {
-            if (images.Count < 1) {
-                MessageBox.Show("Модель не загружена"); 
-                return; 
-            }
-            SaveFileDialog dialog = new SaveFileDialog();
-            dialog.Filter = "HTML files (*.html)|*.html";
-            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (images.Count < 1)
             {
-                ExportToHTML(dialog.FileName);
+                MessageBox.Show("Модель не загружена");
+                return;
+            }
+            try
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "HTML files (*.html)|*.html";
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    ExportToHTML(dialog.FileName);
+                }
+            }
+            catch {
+                MessageBox.Show("Ошибка при сохранении файла! Проверьте количество свободного места на диске.");
             }
         }
 
@@ -204,7 +240,11 @@ namespace _3D_Photo
         {
             auto_scroll_Disabled();
             left_scroll.Background = Brushes.Transparent;
+            left_scroll.Tag = "Unchecked";
+            left_scroll.BorderThickness = new Thickness(1);
             right_scroll.Background = Brushes.Transparent;
+            right_scroll.Tag = "Unchecked";
+            right_scroll.BorderThickness = new Thickness(1);
             scroll_direction = 0;
         }
 
@@ -216,37 +256,38 @@ namespace _3D_Photo
         Thread scroll_thread;
         private void btn_scroll_Click(object sender, MouseButtonEventArgs e)
         {
-            Clear_Scroll();
-            if (images_box.Items.Count < 1) return;
-           
-            Border bord = sender as Border;
-            bord.Tag = "Check";
-            if(bord.Name == "right_scroll") scroll_direction = -1;
-            else if(bord.Name == "left_scroll") scroll_direction = 1;
-            Console.WriteLine(scroll_direction);
-            auto_scroll_enabled = true;
-            curIndex = images_box.SelectedIndex;
+            try
+            {
+                Clear_Scroll();
+                if (images_box.Items.Count < 1) return;
 
-            scroll_thread = new Thread(Img_Timer_Change);
+                Border bord = sender as Border;
+                bord.Tag = "Checked";
+                bord.Background = Brushes.LightBlue;
+                bord.BorderThickness = new Thickness(2);
+                if (bord.Name == "right_scroll") scroll_direction = -1;
+                else if (bord.Name == "left_scroll") scroll_direction = 1;
+                auto_scroll_enabled = true;
 
-            scroll_thread.Start();
+                if (scroll_thread != null)
+                    Console.WriteLine("ST:" + scroll_thread.ManagedThreadId);
 
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += Timer_Tick;
-            timer.Start();
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            Console.WriteLine(scroll_thread.ThreadState);
+                scroll_thread = new Thread(Img_Timer_Change);
+                scroll_thread.Start();
+            }
+            catch { }
         }
 
         private void btn_scroll_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
             Border bord = sender as Border;
-            if (bord.Tag.ToString() == "Check") return;
+            if (bord.Tag.ToString() == "Checked") return;
             bord.Background = Brushes.Transparent;
+        }
+        private void sens_bar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sens = sens_bar.Value;
+            sens_tb.Text = sens.ToString("0.0");
         }
     }
 }
