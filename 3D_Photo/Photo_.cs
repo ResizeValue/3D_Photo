db20073c99b6ecefb87754e1da655527f36bb60d
user <user@DESKTOP-FQJQCDG>
2021-08-01 19:19:45 +0300

    Revert "Version 1.2 Watermark"
    This reverts commit 2a691660f30f1821fc796b6efacd5e7ea4496867.

--- 3D_Photo/Photo_.cs
+++ 3D_Photo/Photo_.cs
@@ -1,7 +1,5 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,38 +11,5 @@ namespace _3D_Photo
         public string Name { get; set; }
         public string Path { get; set; }
         public int Position { get; set; }
-    }
-
-    public class Settings
-    {
-        public double Sensitivity { get; set; }
-        public bool Watermark { get; set; }
-
-        public void Save_Settings()
-        {
-            string jsonString = JsonConvert.SerializeObject(this);
-            File.WriteAllText(Environment.CurrentDirectory + "\\Settings.json", jsonString);
-        }
-        public void Read_Settings()
-        {
-            if (!File.Exists(Environment.CurrentDirectory + "\\Settings.json")) Set_Default_Settings();
-            else
-            {
-                var str = File.ReadAllText(Environment.CurrentDirectory + "\\Settings.json");
-                Settings set = JsonConvert.DeserializeObject<Settings>(str);
-                if (set == null)
-                    Set_Default_Settings();
-                else
-                {
-                    Sensitivity = set.Sensitivity;
-                    Watermark = set.Watermark;
-                }
-            }
-        }
-        public void Set_Default_Settings()
-        {
-            Sensitivity = 50.0;
-            Watermark = false;
-        }
     }
 }
